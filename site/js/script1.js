09ab4fbe11e7ad193320eb239e8d5a1ddc136b24
Rakshit Bhat <rakshitb44@gmail.com>
2024-05-27 22:41:33 +0530

    Your commit message here

---
+++ site/js/script1.js
@@ -0,0 +1,9 @@
+// script.js
+window.addEventListener('load', function() {
+    const preloader = document.querySelector('.preloader');
+    preloader.classList.add('loaded');
+  
+    const content = document.querySelector('.content');
+    content.style.display = 'block'; // Show the main content
+  });
+  
